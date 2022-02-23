1cad05218ceef79cd012e814311bf5e135e2308b
Philipp Maier <pmaier1971@gmail.com>
2022-02-23 08:40:45 -0500

    Initial Commit

---
+++ ProofOfConcept/ProofOfConceptApp.swift
@@ -0,0 +1,17 @@
+//
+//  ProofOfConceptApp.swift
+//  ProofOfConcept
+//
+//  Created by Philipp Maier on 2/23/22.
+//
+
+import SwiftUI
+
+@main
+struct ProofOfConceptApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
